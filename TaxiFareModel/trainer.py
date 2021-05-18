21f3c97f327d8435f4d2d652fe9cd4b17256a70f
Martin Jauffret <martinjfrt@hotmail.fr>
2021-05-18 18:30:47 +0200

    trainer

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -7,9 +7,12 @@ from memoized_property import memoized_property
 from  mlflow.tracking import MlflowClient
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,Lasso
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+import joblib
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
 
 class Trainer(BaseEstimator, TransformerMixin):
     def __init__(self, X, y):
@@ -21,7 +24,8 @@ class Trainer(BaseEstimator, TransformerMixin):
         self.X = X
         self.y = y
         self.experiment_name = "[FR] [Paris] [MartinJ9678] taxifare_model 0"
-        # self.mlflow_cl=None
+        self.best_model=None
+        self.best_params=None
     
     @memoized_property
     def mlflow_client(self):
@@ -36,15 +40,15 @@ class Trainer(BaseEstimator, TransformerMixin):
         except BaseException:
             return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
 
-    @memoized_property
     def mlflow_run(self):
-        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+        self.mlflow_run_object = self.mlflow_client.create_run(self.mlflow_experiment_id)
+        return self.mlflow_run_object
 
     def mlflow_log_param(self, key, value):
-        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+        self.mlflow_client.log_param(self.mlflow_run_object.info.run_id, key, value)
 
     def mlflow_log_metric(self, key, value):
-        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+        self.mlflow_client.log_metric(self.mlflow_run_object.info.run_id, key, value)
     
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -62,27 +66,52 @@ class Trainer(BaseEstimator, TransformerMixin):
                                         ], remainder="drop")
         pipe = Pipeline([
                             ('preproc', preproc_pipe),
-                            ('linear_model', LinearRegression())
+                            ('linear_model', RandomForestRegressor(n_jobs=-1))
                         ])
         return pipe        
 
     def run(self):
         """set and train the pipeline"""
         self.pipeline = self.set_pipeline()
-        self.pipeline.fit(self.X, self.y)
+        grid={
+            'linear_model__max_depth':[2,3],
+            'linear_model__min_samples_leaf': [1,2],
+            'linear_model__n_estimators': [100,150,200]
+        }
+        grid_search = GridSearchCV(self.pipeline,param_grid=grid,n_jobs=-1,cv=5,scoring='neg_mean_squared_error')
+        grid_search.fit(self.X,self.y)
+        self.best_model = grid_search.best_estimator_
+        self.best_params = grid_search.best_params_
+        #self.pipeline.fit(self.X, self.y)
         return self
 
     def evaluate(self, X_test, y_test):
         """evaluates the pipeline on df_test and return the RMSE"""
-        y_pred = self.pipeline.predict(X_test)
+        y_pred = self.best_model.predict(X_test)
+        #y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        # for model in ["a", "b"]: on peut itérer sur des modèles avec des runs différents en bouclant 
+        #     self.mlflow_run()
+        #     self.mlflow_log_metric("rmse",rmse)
+        #     self.mlflow_log_param("model",self.pipeline.get_params()['linear_model'])
+        #     self.mlflow_log_param("truc",model)
+        #     for key,value in self.best_params.items():
+        #         self.mlflow_log_param(key,value)
+        self.mlflow_run()
         self.mlflow_log_metric("rmse",rmse)
         self.mlflow_log_param("model",self.pipeline.get_params()['linear_model'])
+        for key,value in self.best_params.items():
+            self.mlflow_log_param(key,value)
         return rmse
+    
+    def save_model(self,score,model):
+        """ Save the trained model into a model.joblib file """
+        joblib.dump(self.pipeline, f'model.joblib--{model}-{score}')
+        return self
         
 if __name__ == "__main__":
     # get data
-    data = get_data(nrows=1000)
+    data = get_data(nrows=10000)
     # clean data
     data = clean_data(data)
     # set X and y
@@ -94,4 +123,7 @@ if __name__ == "__main__":
     trainer.run()
     # evaluate
     score = trainer.evaluate(X_val,y_val)
-    
+    # joblib
+    model=trainer.pipeline.get_params()['linear_model']
+    trainer.save_model(score,model)
+    print(trainer.best_params)
