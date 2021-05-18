fce5a004f2cc41e5ae19b7c5fcc566b61c8e5bb3
Martin Jauffret <martinjfrt@hotmail.fr>
2021-05-18 15:45:18 +0200

    MLflow start

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,17 +1,15 @@
 # imports
-
 from TaxiFareModel.data import clean_data, get_data, getXy, getholdout
-
 from TaxiFareModel.utils import compute_rmse
-
 from TaxiFareModel.encoders import DistanceTransformer,TimeFeaturesEncoder
+import mlflow
+from memoized_property import memoized_property
+from  mlflow.tracking import MlflowClient
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-
-from sklearn.metrics import r2_score
 
 class Trainer(BaseEstimator, TransformerMixin):
     def __init__(self, X, y):
@@ -22,6 +20,31 @@ class Trainer(BaseEstimator, TransformerMixin):
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = "[FR] [Paris] [MartinJ9678] taxifare_model 0"
+        # self.mlflow_cl=None
+    
+    @memoized_property
+    def mlflow_client(self):
+        MLFLOW_URI = "https://mlflow.lewagon.co/"
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+    
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
     
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -53,6 +76,8 @@ class Trainer(BaseEstimator, TransformerMixin):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
         rmse = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric("rmse",rmse)
+        self.mlflow_log_param("model",self.pipeline.get_params()['linear_model'])
         return rmse
         
 if __name__ == "__main__":
@@ -69,4 +94,4 @@ if __name__ == "__main__":
     trainer.run()
     # evaluate
     score = trainer.evaluate(X_val,y_val)
-    print(score)
+    
